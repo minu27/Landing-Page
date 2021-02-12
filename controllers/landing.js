1db11a4e74af7dd3aff538b216f299d884c81d18
Nikhil Bhasin <nikhilbhasin@Nikhils-Air.Dlink>
2021-02-12 17:40:31 +0530

    Contact Submission Added

--- controllers/landing.js
+++ controllers/landing.js
@@ -4,7 +4,8 @@ exports.get_landing = function(req, res, next) {
     res.sendFile('index.html', { root: 'views' });
 }
 exports.post_contact = function(req, res, next) {
-        console.log(req.body);
+    console.log(req.body);
     
     console.log("Request Recieved");
+    res.sendFile('index.html', { root: 'views' });
 }
